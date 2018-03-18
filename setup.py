95f0b81344de402396d894331aceef2e454e2a3a
Roman Miroshnychenko <roman1972@gmail.com>
2018-03-18 23:59:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # Author: Roman Miroshnychenko aka Roman V.M.
 # E-mail: roman1972@gmail.com
+# License: MIT, see LICENSE.txt
 
 from io import open
 try:
@@ -16,11 +17,24 @@ setup(
     name='asyncore-wsgi',
     version=asyncore_wsgi.__version__,
     packages=['asyncore_wsgi'],
-    url='',
+    url='https://github.com/romanvm/asyncore-wsgi',
     license='MIT',
     author='Roman Miroshnychenko',
     author_email='roman1972@gmail.com',
     description='Asynchronous WSGI and WebSocket server based on asyncore module',
     long_description=long_descr,
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server'
+    ],
 )

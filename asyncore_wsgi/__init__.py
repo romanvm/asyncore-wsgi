06a1e2541f4c73230a58b33c00050f43500f09f2
Roman Miroshnychenko <roman1972@gmail.com>
2018-08-02 00:07:21 +0300

    Version bump: 0.0.4

--- asyncore_wsgi/__init__.py
+++ asyncore_wsgi/__init__.py
@@ -45,7 +45,7 @@ from .SimpleWebSocketServer import AsyncWebSocketHandler
 __all__ = ['AsyncWsgiHandler', 'AsyncWebSocketHandler', 'AsyncWsgiServer',
             'make_server']
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 logging.basicConfig(
     format='%(asctime)s: %(name)s - %(module)s:%(lineno)d - %(levelname)s - %(message)s',

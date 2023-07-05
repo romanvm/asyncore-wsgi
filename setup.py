d228ed6c32267fe820b350808cb35dc26f2ed5c8
Roman <roman1972@gmail.com>
2023-07-05 23:35:59 +0300

    Move to modern package layout

--- setup.py
+++ setup.py
@@ -3,38 +3,6 @@
 # E-mail: roman1972@gmail.com
 # License: MIT, see LICENSE.txt
 
-from io import open
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-import asyncore_wsgi
+from setuptools import setup
 
-with open('Readme.rst', 'r', encoding='utf-8') as fo:
-    long_descr = fo.read()
-
-setup(
-    name='asyncore-wsgi',
-    version=asyncore_wsgi.__version__,
-    packages=['asyncore_wsgi'],
-    url='https://github.com/romanvm/asyncore-wsgi',
-    license='MIT',
-    author='Roman Miroshnychenko',
-    author_email='roman1972@gmail.com',
-    description='Asynchronous WSGI and WebSocket server based on asyncore module',
-    long_description=long_descr,
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server'
-    ],
-)
+setup()
